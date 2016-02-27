46bdcefd2a2cd0f4bd2ccf5f636ea82cd9dc85ba
Copyes <fan_545697167@qq.com>
2016-02-27 20:52:25 +0800

    增加图片操纵方法

--- lightbox/lightbox.js
+++ lightbox/lightbox.js
@@ -84,12 +84,110 @@
 				top:(winHeight-viewHight)/2
 
 			},function(){
-
+				//加载图片
+				self.loadPicSize(sourceSrc);
 			});	
+		//根据当前点击的元素id获取在当前图片组里面的索引
+		
 
+		this.index = this.getIndexOf(curId);
+		
+		var groupDataLen = this.groupData.length;
+		if(groupDataLen>1){
+			if(this.index === 0){
+				this.btnPrev.addClass('disabled');
+				this.btnNext.removeClass('disabled');
+			}else if(this.index === groupDataLen-1){
+				this.btnPrev.removeClass('disabled');
+				this.btnNext.addClass('disabled');
+			}else{
+				this.btnNext.removeClass('disabled');
+				this.btnPrev.removeClass('disabled');
+			}
+		}
+		console.log(this.index);
+		this.openWin.fadeIn();
+
+		},
+
+		loadPicSize : function(sourceSrc){
+			var self = this;
+
+			this.preLoadImag(sourceSrc,function(){
+
+				self.popImg.attr('src', sourceSrc);
+
+				var picWidth = self.popImg.width(),
+				picHeight = self.popImg.height();
+
+				self.changePic(picWidth,picHeight);
+			});
+		},
+		changePic:function(picWidth,picHeight){
+			var self = this,
+				winHeight = $(window).height(),
+				winWidth = $(window).width();
+
+			//如果图片的宽高大于浏览器的宽高
 			
-			this.openWin.fadeIn();
+			var scale = Math.min(winWidth/(picWidth+10),winHeight/(picHeight+10),10);
 
+			picWidth = picWidth*scale;
+			picHeight = picHeight*scale;
+
+
+			this.picViewArea.animate({
+				width:picWidth-10,
+				height:picHeight-10
+			});
+
+			this.openWin.animate({
+				width:picWidth,
+				height:picHeight,
+				marginLeft:-(picWidth/2),
+				top:(winHeight-picHeight)/2
+			},function(){
+				self.popImg.css({
+					width:picWidth-10,
+					height:picHeight-10
+				}).fadeIn();
+
+				self.picCaption.fadeIn();
+			});
+
+			//设置文字描述和索引
+			this.wordDesc.text(this.groupData[this.index].caption);
+			this.imgIndex.text('当前索引：'+(this.index+1)+' of ' + this.groupData.length);
+		},
+
+		preLoadImag:function(sourceSrc,callback){
+			var img = new Image();
+			if(!!window.ActiveObject){
+				img.onreadystatechange = function(){
+					if(this.readyState == 'complete'){
+						callback();
+					}
+				}
+			}else{
+				img.onload = function(){
+					callback();
+				}
+			}
+			img.src = sourceSrc;
+		},
+
+		getIndexOf : function(curId){
+
+			var index = 0;
+			$(this.groupData).each(function(i){
+				index = i;
+
+				if(this.id === curId){
+					return false;
+				}
+			});
+
+			return index;
 		},
 
 
@@ -114,7 +212,7 @@
 		renderDom : function(){
 			var strDom = '<div class="lightbox-img">'+
 							'<a href="javascript:;" class="btn prev icon-prev"></a>'+
-							'<img class="images" src="./images/2-2.jpg" alt="" width="100%"／>'+
+							'<img class="images" src="./images/2-2.jpg" alt=""／>'+
 							'<a href="javascript:;" class="btn next icon-next"></a>'+
 						'</div>'+
 						'<div class="lightbox-word">'+
